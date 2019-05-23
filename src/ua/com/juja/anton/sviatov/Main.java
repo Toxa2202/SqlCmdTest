e425b857ae185f37f4287e30b5d3c37fcca3ffd3
anton.sviatov <svyatoj@gmail.com>
2019-05-23 19:10:42 +0300

    Add Commands:   - INSERT   - SELECT   - DELETE   - UPDATE

--- src/ua/com/juja/anton/sviatov/Main.java
+++ src/ua/com/juja/anton/sviatov/Main.java
@@ -1,8 +1,7 @@
 package ua.com.juja.anton.sviatov;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Random;
 
 /**
  * Created by anton.sviatov on 23.05.2019.
@@ -13,5 +12,34 @@ public class Main {
         Connection connection = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5434/sqlcmd", "postgres",
                     "postgres");
+        // insert
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("INSERT INTO public.user (name, password)"
+                + "VALUES ('Stiven', 'Pupkin');");
+
+        // select
+        stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery( "SELECT * FROM public.user WHERE id > 10" );
+        while ( rs.next() ) {
+            System.out.println( "id: " + rs.getString("id"));
+            System.out.println( "name: " + rs.getString("name"));
+            System.out.println( "password: " + rs.getString("password"));
+            System.out.println("-----");
+        }
+        rs.close();
+        stmt.close();
+
+        // delete
+        stmt = connection.createStatement();
+        stmt.executeUpdate("DELETE FROM public.user WHERE id > 10 AND id < 100");
+        stmt.close();
+
+        // update
+        PreparedStatement ps = connection.prepareStatement(
+                "UPDATE public.user SET password = ? WHERE id > 3");
+        String pass = "password_" + new Random().nextInt();
+        ps.setString(1, pass);
+        ps.executeUpdate();
+        ps.close();
     }
 }
