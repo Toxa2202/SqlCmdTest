294d788c3630677c8faa55923c76df941a516268
anton.sviatov <svyatoj@gmail.com>
2019-05-23 17:33:17 +0300

    Create JDBC Connection

--- src/ua/com/juja/anton/sviatov/Main.java
+++ src/ua/com/juja/anton/sviatov/Main.java
@@ -1,7 +1,17 @@
 package ua.com.juja.anton.sviatov;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by anton.sviatov on 23.05.2019.
  */
 public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5434/sqlcmd", "postgres",
+                    "postgres");
+    }
 }
